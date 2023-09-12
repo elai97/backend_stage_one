a9cf81c7e05fb2f6246c483d03af531ba87a3981
elai97 <mailizuchukwu042@gmail.com>
2023-09-12 22:33:36 +0100

    first commit

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ app.get('/api', (req, res) => {
     // Get the current date and time
     const currentDate = new Date();
     const currentDay = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
-    const utcTime = currentDate.toISOString();
+
+    // Format UTC time as "2023-09-12T21:30:29Z"
+    const year = currentDate.getUTCFullYear();
+    const month = String(currentDate.getUTCMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getUTCDate()).padStart(2, '0');
+    const hours = String(currentDate.getUTCHours()).padStart(2, '0');
+    const minutes = String(currentDate.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getUTCSeconds()).padStart(2, '0');
+    const utcTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}Z`;
 
     // GitHub URLs
     const githubFileUrl = 'https://github.com/Ohuegbe01/Praise-app-sample/blob/main/index.js';
